bea34f3eefe044124e5712174d69d46b844a0a18
DerIch69420 <netroMorten@web.de>
2025-05-30 13:50:58 +0200

    finished base game

--- main.lua
+++ main.lua
@@ -2,16 +2,15 @@ local love = require("love")
 
 local game = require("src.game")
 
-local screenWidth, screenheight = love.graphics.getDimensions()
-local cellSize = 2
-local gridWith, gridHeight = screenWidth / cellSize, screenheight / cellSize
-local grid, nextGrid = {}, {}
-
 function love.load()
     math.randomseed(os.time())
-	grid = game.initGrid(gridWith, gridHeight)
+    game:initGrid()
 end
 
-function love.draw() end
+function love.draw()
+    game:draw()
+end
 
-function love.update(dt) end
+function love.update(dt)
+    game:updateGrid()
+end
--- src/game.lua
+++ src/game.lua
@@ -1,23 +1,67 @@
 local Game = {}
 
-function Game.initGrid(gridWith, gridHeight)
-	local grid = {}
+local screenWidth, screenHeight = love.graphics.getDimensions()
 
-	for x = 0, gridWith - 1 do
-		grid[x] = {}
+Game.cellSize = 2
+Game.gridWidth = math.floor(screenWidth / Game.cellSize)
+Game.gridHeight = math.floor(screenHeight / Game.cellSize)
 
-		for y = 0, gridHeight - 1 do
-			grid[x][y] = math.random(0, 1)
-		end
-	end
+Game.grid = {}
+Game.nextGrid = {}
 
-	return grid
+function Game:initGrid()
+    for x = 0, self.gridWidth - 1 do
+        self.grid[x] = {}
+        self.nextGrid[x] = {}
+        for y = 0, self.gridHeight - 1 do
+            self.grid[x][y] = math.random(0, 1)
+            self.nextGrid[x][y] = 0
+        end
+    end
 end
 
-function Game.neighbors() end
+function Game:countNeighbors(x, y)
+    local count = 0
+    for dx = -1, 1 do
+        for dy = -1, 1 do
+            if not (dx == 0 and dy == 0) then
+                local nx, ny = x + dx, y + dy
+                if nx >= 0 and nx < self.gridWidth and ny >= 0 and ny < self.gridHeight then
+                    count = count + self.grid[nx][ny]
+                end
+            end
+        end
+    end
+    return count
+end
 
-function Game.updateGrid() end
+function Game:updateGrid()
+    for x = 0, self.gridWidth - 1 do
+        for y = 0, self.gridHeight - 1 do
+            local neighbors = self:countNeighbors(x, y)
+            local alive = self.grid[x][y]
 
-function Game.showGrid() end
+            if alive == 1 and (neighbors < 2 or neighbors > 3) then
+                self.nextGrid[x][y] = 0
+            elseif alive == 0 and neighbors == 3 then
+                self.nextGrid[x][y] = 1
+            else
+                self.nextGrid[x][y] = alive
+            end
+        end
+    end
+    self.grid, self.nextGrid = self.nextGrid, self.grid
+end
+
+function Game:draw()
+    love.graphics.setColor(1, 1, 1)
+    for x = 0, self.gridWidth - 1 do
+        for y = 0, self.gridHeight - 1 do
+            if self.grid[x][y] == 1 then
+                love.graphics.rectangle("fill", x * self.cellSize, y * self.cellSize, self.cellSize, self.cellSize)
+            end
+        end
+    end
+end
 
 return Game
